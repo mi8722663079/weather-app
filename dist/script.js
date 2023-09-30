3b115139e3cb769cc0ab079c1685436a6585dbeb
mi8722663079 <mi8722663@gmail.com>
2023-09-30 09:12:23 +0300

    learned Jquery and some animation progress

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,14 @@
-"use strict";
-const num = 124;
+$(function () {
+  $(".error").hide();
+  $(".weather").hide();
+  $(".weather *").hide();
+  $(".error *").fadeToggle();
+});
+$("input").on("click", () => {
+  $(".weather").slideToggle();
+  $(".weather *").fadeToggle();
+});
+$("input").on("input", () => {
+  $(".error").slideToggle(300);
+  $(".error *").fadeToggle(300);
+});
