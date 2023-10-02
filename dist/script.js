a33b5d744473ab5db8486e570b0954e9f4e4cb93
mi8722663079 <mi8722663@gmail.com>
2023-10-02 08:16:09 +0200

    progress

--- dist/script.js
+++ dist/script.js
@@ -3,12 +3,13 @@ $(function () {
   $(".weather").hide();
   $(".weather *").hide();
   $(".error *").fadeToggle();
-});
-$("input").on("click", () => {
-  $(".weather").slideToggle();
-  $(".weather *").fadeToggle();
-});
-$("input").on("input", () => {
-  $(".error").slideToggle();
-  $(".error *").fadeToggle();
+
+  $(".search").on("click", () => {
+    $(".search").animate({ width: "80vw" });
+    $(".search input").animate({ opacity: 1 });
+  });
+  $("input").on("click", () => {
+    $(".weather").slideToggle();
+    $(".weather *").fadeToggle(1000);
+  });
 });
