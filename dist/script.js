f932f943825fc30347a10524c5cccc2979460182
mi8722663079 <mi8722663@gmail.com>
2023-10-03 17:47:19 +0200

    progress

--- dist/script.js
+++ dist/script.js
@@ -3,13 +3,16 @@ $(function () {
   $(".weather").hide();
   $(".weather *").hide();
   $(".error *").fadeToggle();
-
-  $(".search").on("click", () => {
-    $(".search").animate({ width: "80vw" });
-    $(".search input").animate({ opacity: 1 });
-  });
-  $("input").on("click", () => {
-    $(".weather").slideToggle();
-    $(".weather *").fadeToggle(1000);
+  $(".search").animate({ width: "80vw" }, 500);
+  $(".search input").animate({ opacity: 1 }, 500);
+  $("input").keypress(function (e) {
+    const enter = e.keyCode ? e.keyCode : e.which;
+    if (enter == "13" && this.value == "weather") {
+      $(".weather *").fadeToggle(1000);
+      $(".weather").slideToggle();
+    } else if (enter == "13" && this.value == "error") {
+      $(".error *").fadeToggle(1000);
+      $(".error").slideToggle();
+    }
   });
 });
