6239370b632bbe130fcb25e0e9e46160575b12f8
mi8722663079 <mi8722663@gmail.com>
2023-10-04 10:24:43 +0300

    done!!!!

--- dist/script.js
+++ dist/script.js
@@ -1,18 +1,51 @@
+const search = document.querySelector("input");
+const image = document.querySelector(".status img");
+
+async function getWeather(city) {
+  try {
+    const weather = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=metric&key=66XZLFZ7AYETUWM59UFL4TRCF&contentType=json`
+    );
+    const response = await weather.json();
+    $(".weather *").fadeToggle();
+    $(".weather").slideToggle();
+    return response;
+  } catch {
+    $(".error *").fadeToggle();
+    $(".error").slideToggle();
+  }
+}
 $(function () {
   $(".error").hide();
   $(".weather").hide();
   $(".weather *").hide();
-  $(".error *").fadeToggle();
+  $(".error *").hide();
   $(".search").animate({ width: "80vw" }, 500);
   $(".search input").animate({ opacity: 1 }, 500);
   $("input").keypress(function (e) {
     const enter = e.keyCode ? e.keyCode : e.which;
-    if (enter == "13" && this.value == "weather") {
-      $(".weather *").fadeToggle(1000);
-      $(".weather").slideToggle();
-    } else if (enter == "13" && this.value == "error") {
-      $(".error *").fadeToggle(1000);
-      $(".error").slideToggle();
+
+    if (enter == "13") {
+      let conditions = getWeather(this.value).then((r) => {
+        console.log(r);
+        image.setAttribute("src", `icons/${r.days[0].icon}.svg`);
+        $(".weather h1").text(r.resolvedAddress.split(",")[0]);
+        $(".weather h2").text(r.currentConditions.conditions);
+        $(".weather h3").text(`${r.currentConditions.temp}°`);
+        $(".status p").text(
+          `L: ${r.days[0].tempmin}°   H: ${r.days[0].tempmax}°`
+        );
+        $(".wind p").text(`wind speed: ${r.currentConditions.windspeed} m/s`);
+        $(".Temp p").text(`Avg Temp: ${r.days[0].temp}°`);
+        $(".vis p").text(`Avg visibility: ${r.days[0].visibility} km`);
+        $(".hum p").text(`Avg humidity: ${r.days[0].humidity}%`);
+      });
     }
+  });
+  $("input").on("input", () => {
+    $(".error *").fadeOut();
+    $(".error").slideUp();
+    $(".weather *").fadeOut();
+    $(".weather").slideUp();
   });
 });
